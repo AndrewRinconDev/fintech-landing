41eeeff01601527db788bbfb3855bb71e130406b
Andres Rincon <andres.rincon@antechdx.com>
2025-04-16 14:04:20 -0500

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563eb',
+          dark: '#1d4ed8'
+        },
+        secondary: {
+          DEFAULT: '#059669',
+          dark: '#047857'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
